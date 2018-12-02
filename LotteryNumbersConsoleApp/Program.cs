4586c4c09f6a27d898ce6d69b6b3a7dd66cffce1
Paul Adams <aeriandi@takatakatum.com>
2018-12-02 15:36:46 +0000

    initial commit

---
+++ LotteryNumbersConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryNumbersConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
