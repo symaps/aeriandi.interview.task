34f6d87d42276f7e7c7da823c25b2cb503f92131
Paul Adams <aeriandi@takatakatum.com>
2018-12-02 16:40:52 +0000

    Added code to generate numbers and changed naming convention(was using naming convention I use for c#

--- LotteryNumbersJs/LotteryNumbersJs/Scripts/LotteryNumberGenerator.js
+++ LotteryNumbersJs/LotteryNumbersJs/Scripts/LotteryNumberGenerator.js
@@ -1,20 +1,35 @@
 ﻿var numberGenerator = function() {
-    var _ballBank = []; // all the available balls. Balls can be in one of two states not selected or selected. This is analogus to being inside the ball machine or on the display rack.
-    var _resetBallBank = function () {
+    var ballBank = []; // all the available balls. Balls can be in one of two states not selected or selected. This is analogus to being inside the ball machine or on the display rack.
+    var resetBallBank = function () {
         var i = 0;
-        var _totalBallCount = 49;
-        _ballBank = [];
-        for (i = 0; i < _totalBallCount; ++i) {
-            _ballBank.push({ number: i + 1, selected: false });
+        var totalBallCount = 49;
+        ballBank = [];
+        for (i = 0; i < totalBallCount; ++i) {
+            ballBank.push({ number: i + 1, selected: false });
         }
     };
+    var pickBall = function () {
+        var ballNum = Math.floor((Math.random() * ballBank.length));
+        while (ballBank[ballNum].selected) { // we might select a ball that has already been picked. if so lets try again. 
+            ballNum = Math.floor((Math.random() * ballBank.length));
+        }
+        ballBank[ballNum].selected = true;
+    };
+    var generateNumbers = function () {
+        var i = 0;
+        for (i = 0; i < selectBallCount; ++i) {
+            pickBall();
+        }
+        
+
+    };
     return {
-        init: function (btn) {
-            
-            btn.on("click", function () {
+        init: function (btn_, selectBallCount_) {
+            selectBallCount = selectBallCount_;
+            btn_.on("click", function () {
                 var i = 0;
-                _resetBallBank();
-                alert("click");
+                resetBallBank();
+                generateNumbers();
             });
         }
 
