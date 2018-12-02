53374a588913d22214e65a9b3a9a57a1429fda21
Paul Adams <aeriandi@takatakatum.com>
2018-12-02 16:28:52 +0000

    Code added to initialise ball bank

--- LotteryNumbersJs/LotteryNumbersJs/Scripts/LotteryNumberGenerator.js
+++ LotteryNumbersJs/LotteryNumbersJs/Scripts/LotteryNumberGenerator.js
@@ -1,1 +1,18 @@
-﻿alert("hi");
+﻿var numberGenerator = function() {
+    var _ballBank = []; // all the available balls. Balls can be in one of two states not selected or selected. This is analogus to being inside the ball machine or on the display rack.
+    var _resetBallBank = function () {
+        var i = 0;
+        var _totalBallCount = 49;
+        _ballBank = [];
+        for (i = 0; i < _totalBallCount; ++i) {
+            _ballBank.push({ number: i + 1, selected: false });
+        }
+    };
+    return {
+        init: function () {
+            
+            _resetBallBank();
+        }
+
+    };
+}
